e29351cd7fd93251ab83bcd36e9e09e1e43b3de4
bakomon <mybakomon@gmail.com>
2024-07-02 07:57:51 +0800

    1.5

--- api/index.php
+++ api/index.php
@@ -2,19 +2,20 @@
 
 namespace Api;
 
-require_once __DIR__ . '/Services/Route.php';
-require_once __DIR__ . '/CController.php';
 require_once __DIR__ . '/Allowed.php';
+require_once __DIR__ . '/Services/Route.php';
+require_once __DIR__ . '/Controllers/Controller.php';
 
-use Api\Services\Route;
-use Api\CController;
 use Api\Allowed;
+use Api\Services\Route;
+use Api\Controllers\Controller;
 
 $route = new Route;
 
-if ((new Allowed)->check() || isset($_GET['dev'])) :
+// Prevent direct url access
+if ((new Allowed)->check()) :
     if ($route->param_check('index', $_GET)) :
-        $route->get($_GET['index'], CController::class, $_GET['index'] . 'Page');
+        $route->get($_GET['index'], Controller::class, $_GET['index'] . 'Page');
     endif;
 else :
     http_response_code(403);
